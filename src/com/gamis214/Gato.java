71c7664ff4164a1fc16e0979b9b7dae52d17275f
Gamis214 <gamis214@gmail.com>
2015-12-22 11:54:54 -0600

    Commit Gral, cambios

---
+++ src/com/gamis214/Gato.java
@@ -0,0 +1,24 @@
+package com.gamis214;
+
+
+public class Gato extends Animal {
+
+    String raza;
+
+    public Gato(String nombre, String tipo_alimentacion, int edad, String raza) {
+        super(nombre, tipo_alimentacion, edad);
+        this.raza = raza;
+    }
+
+    public String getRaza() {
+        return raza;
+    }
+
+    public void setRaza(String raza) {
+        this.raza = raza;
+    }
+
+    public void mostrar(){
+        System.out.println(getNombre() + "-"+getTipo_alimentacion()+"-"+getEdad()+"-"+getRaza());
+    }
+}
--- src/com/gamis214/Main.java
+++ src/com/gamis214/Main.java
@@ -5,9 +5,15 @@ public class Main {
     public static void main(String[] args) {
 
         Perro perro = new Perro("Teddy","Croquetas",10,"Chihuahua");
+        Gato gato = new Gato("Pelusa","Especial",8,"Siames");
+        Caballo caballo = new Caballo("Jhonny","Pasto",21,"Fino");
 
         //-->Nos muestra los detalles del objeto
         perro.mostrar();
+        System.out.println("--------------------------------------------------");
+        gato.mostrar();
+        System.out.println("--------------------------------------------------");
+        caballo.mostrar();
 
     }
 }
