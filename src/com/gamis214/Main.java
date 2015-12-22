9ff7539ae4e72a7607cc09b0d63fbbcb15ed133c
Gamis214 <gamis214@gmail.com>
2015-12-22 11:36:04 -0600

    first commit

---
+++ src/com/gamis214/Main.java
@@ -0,0 +1,13 @@
+package com.gamis214;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        Perro perro = new Perro("Teddy","Croquetas",10,"Chihuahua");
+
+        //-->Nos muestra los detalles del objeto
+        perro.mostrar();
+
+    }
+}
---
+++ src/com/gamis214/Perro.java
@@ -0,0 +1,24 @@
+package com.gamis214;
+
+
+public class Perro extends Animal {
+
+    private String raza;
+
+    public Perro(String nombre,String tipo_alimentacion,int edad,String raza){
+        super(nombre,tipo_alimentacion,edad);
+        this.raza = raza;
+    }
+
+    public String getRaza() {
+        return raza;
+    }
+
+    public void setRaza(String raza) {
+        this.raza = raza;
+    }
+
+    public void mostrar(){
+        System.out.println(getNombre() + "-"+getTipo_alimentacion()+"-"+getEdad()+"-"+getRaza());
+    }
+}
